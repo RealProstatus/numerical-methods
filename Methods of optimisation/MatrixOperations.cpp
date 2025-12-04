4612e157989bd40e3f135a1eec1685d82e805698
Ivan Melnik <mel.sirvania@gmail.com>
2025-12-04 20:06:22 +0300

    Upd include directories and released Eigen Pade

--- Methods of optimisation/MatrixOperations.cpp
+++ Methods of optimisation/MatrixOperations.cpp
@@ -8,6 +8,9 @@
 #include <mkl_lapacke.h>
 #include <set>
 
+#include <Eigen/Dense>
+#include <unsupported/Eigen/MatrixFunctions>
+
 namespace matrix_ops {
     // 1. Базовые матричные операции
     void matmul(const CMatrix& A, const CMatrix& B, CMatrix& C, int N,
@@ -701,5 +704,30 @@ namespace matrix_ops {
         }
 
         return max_abs_eig;
+    }
+
+    CMatrix expm_pade_eigen(const CMatrix& A, int N) {
+        using namespace Eigen;
+
+        // Convert CMatrix into Eigen MatrixXcd
+        MatrixXcd eigen_A(N, N);
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                eigen_A(i, j) = A[i * N + j];
+            }
+        }
+
+        // Comuting exp(A) with Pade + scaling/squaring
+        MatrixXcd exp_result = eigen_A.exp();
+
+        // Convert result
+        CMatrix result(N * N);
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                result[i * N + j] = exp_result(i, j);
+            }
+        }
+
+        return result;
     }
 }
