4b84509bbb7b3565a483bc93a4e914d32c7fa2ef
Ivan Melnik <mel.sirvania@gmail.com>
2026-05-10 16:50:15 +0300

    314Magnus uses inplace commutator

--- src/core/magnus/Formula314Magnus.cpp
+++ src/core/magnus/Formula314Magnus.cpp
@@ -43,127 +43,74 @@ namespace matrix_ops {
         auto zero = [&]() { return CMatrix((size_t)N * N, complexd(0.0, 0.0)); };
 
         // ===== Compute commutators according to (3.15) =====
+
+        // Pre-allocate
+        CMatrix C1(N * N), C2(N * N), C3(N * N), C4(N * N);
+        CMatrix C5(N * N), C6(N * N), C7(N * N), C8(N * N), C9(N * N);
+
         // C1 = [H0, Hmod]
-        CMatrix C1 = commutator(H0, Hmod, N);
+        commutator_inplace(H0, Hmod, C1, N);
 
         // C2 = [H0, C1], C3 = [Hmod, C1]
-        CMatrix C2 = commutator(H0, C1, N);
-        CMatrix C3 = commutator(Hmod, C1, N);
+        commutator_inplace(H0, C1, C2, N);
+        commutator_inplace(Hmod, C1, C3, N);
 
         // C4 = [H0, C2], C5 = [Hmod, C2]
-        CMatrix C4 = commutator(H0, C2, N);
-        CMatrix C5 = commutator(Hmod, C2, N);
+        commutator_inplace(H0, C2, C4, N);
+        commutator_inplace(Hmod, C2, C5, N);
 
         // C6 = [H0, C3], C7 = [Hmod, C3]
-        CMatrix C6 = commutator(H0, C3, N);
-        CMatrix C7 = commutator(Hmod, C3, N);
+        commutator_inplace(H0, C3, C6, N);
+        commutator_inplace(Hmod, C3, C7, N);
 
         // C8 = [C1, C2], C9 = [C1, C3]
-        CMatrix C8 = commutator(C1, C2, N);
-        CMatrix C9 = commutator(C1, C3, N);
+        commutator_inplace(C1, C2, C8, N);
+        commutator_inplace(C1, C3, C9, N);
 
         // ===== Begin constructing Omega =====
         CMatrix Omega = zero();
 
-        auto add = [&](const CMatrix& M) { mat_add(Omega, M, Omega, N); };
+        auto add_scaled = [&](CMatrix& M, double scalar) {
+            mat_axpy(M, Omega, N, complexd(scalar, 0.0)); // Используем быстрый axpy вместо scale + add
+        };
 
         // 1) h H0
-        {
-            CMatrix term = H0;
-            mat_scale_inplace(term, N, complexd(h, 0.0));
-            add(term);
-        }
+        add_scaled(const_cast<CMatrix&>(H0), h);
 
         // 2) h * ( f(t1) + (h^2/24) f''(t1/2) ) * Hmod
-        {
-            double c = f_t1 + (h * h / 24.0) * f2_half;
-            CMatrix term = Hmod;
-            mat_scale_inplace(term, N, complexd(h * c, 0.0));
-            add(term);
-        }
+        add_scaled(const_cast<CMatrix&>(Hmod), h * (f_t1 + (h * h / 24.0) * f2_half));
 
         // 3) - (1/12) h^3 f'(t1/2) C1
-        {
-            CMatrix term = C1;
-            double c = -(h * h * h / 12.0) * fp_half;
-            mat_scale_inplace(term, N, complexd(c, 0));
-            add(term);
-        }
+        add_scaled(C1, -(h * h * h / 12.0) * fp_half);
 
         // 4) + (1/7200) h^5 f''(t1/2) * C2
-        {
-            CMatrix term = C2;
-            double c = (h * h * h * h * h / 7200.0) * f2_half;
-            mat_scale_inplace(term, N, complexd(c, 0));
-            add(term);
-        }
+        double h5 = h * h * h * h * h;
+        add_scaled(C2, (h5 / 7200.0) * f2_half);
 
         // 5) + (1/240) h^5 * ( - (f'(t1/2))^2 ) * Hmod
-        {
-            CMatrix term = Hmod;
-            double c = (h * h * h * h * h / 240.0) * (-fp_half * fp_half);
-            mat_scale_inplace(term, N, complexd(c, 0));
-            add(term);
-        }
+        add_scaled(const_cast<CMatrix&>(Hmod), (h5 / 240.0) * (-fp_half * fp_half));
 
         // 6) + (1/30) h^5 f''(t1/2)*( f(t1/2)+1/4 h^2 f''(t1/2) ) * C3
-        {
-            double s = f_half + 0.25 * h * h * f2_half;
-            double c = (h * h * h * h * h / 30.0) * (f2_half * s);
-            CMatrix term = C3;
-            mat_scale_inplace(term, N, complexd(c, 0));
-            add(term);
-        }
+        add_scaled(C3, (h5 / 30.0) * f2_half * (f_half + 0.25 * h * h * f2_half));
 
         // 7) + (1/7200) h^5 f'(t1/2) * C4
-        {
-            double c = (h * h * h * h * h / 7200.0) * fp_half;
-            CMatrix term = C4;
-            mat_scale_inplace(term, N, complexd(c, 0));
-            add(term);
-        }
+        add_scaled(C4, (h5 / 7200.0) * fp_half);
 
         // 8) + (1/7200) h^5 ( f(t1/2) f'(t1/2) + 1/4 h^2 f''(t1/2) ) * C5
-        {
-            double s = f_half * fp_half + 0.25 * h * h * f2_half;
-            double c = (h * h * h * h * h / 7200.0) * s;
-            CMatrix term = C5;
-            mat_scale_inplace(term, N, complexd(c, 0));
-            add(term);
-        }
+        add_scaled(C5, (h5 / 7200.0) * (f_half * fp_half + 0.25 * h * h * f2_half));
 
         // 9) + (1/7200) h^5 f(t1/2) f'(t1/2) * C6
-        {
-            double c = (h * h * h * h * h / 7200.0) * (f_half * fp_half);
-            CMatrix term = C6;
-            mat_scale_inplace(term, N, complexd(c, 0));
-            add(term);
-        }
+        add_scaled(C6, (h5 / 7200.0) * f_half * fp_half);
 
         // 10) + (1/7200) h^5 f(t1/2) f'(t1/2) ( f(t1) + 1/4 h^2 f''(t1/2) ) * C7
-        {
-            double s = f_t1 + 0.25 * h * h * f2_half;
-            double c = (h * h * h * h * h / 7200.0) * (f_half * fp_half * s);
-            CMatrix term = C7;
-            mat_scale_inplace(term, N, complexd(c, 0));
-            add(term);
-        }
+        add_scaled(C7, (h5 / 7200.0) * (f_half * fp_half * (f_t1 + 0.25 * h * h * f2_half)));
 
         // 11) - (1/14400) h^7 (f'(t1/2))^2 * C8
-        {
-            double c = -(h * h * h * h * h * h * h / 14400.0) * (fp_half * fp_half);
-            CMatrix term = C8;
-            mat_scale_inplace(term, N, complexd(c, 0));
-            add(term);
-        }
+        double h7 = h5 * h * h;
+        add_scaled(C8, -(h7 / 14400.0) * (fp_half * fp_half));
 
         // 12) - (1/14400) h^7 (f'(t1/2))^2 f(t1/2) * C9
-        {
-            double c = -(h * h * h * h * h * h * h / 14400.0) * (fp_half * fp_half * f_half);
-            CMatrix term = C9;
-            mat_scale_inplace(term, N, complexd(c, 0));
-            add(term);
-        }
+        add_scaled(C9, -(h7 / 14400.0) * (fp_half * fp_half * f_half));
 
         return Omega;
     }
